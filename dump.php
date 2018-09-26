9e5892fc0e8f1ac3e065a743b751b5244f0b023c
Hashman Lin <coosos810609@gmail.com>
2018-09-26 23:32:01 +0800

    Add slack retry error notification

--- dump.php
+++ dump.php
@@ -1,5 +1,6 @@
 <?php
 
+date_default_timezone_set("Asia/Taipei");
 $config_file = __DIR__ . '/config.php';
 if (!file_exists($config_file)) {
     echo "Config file not found\n";
@@ -13,15 +14,19 @@ require __DIR__ . '/vendor/autoload.php';
 use Mailgun\Mailgun;
 use Mailgun\Api\Event;
 
-$begin = date("D, d M Y H:i:00 +0800", strtotime("-10 minutes"));
+$begin = date("D, d M Y H:i:00 +0800", strtotime("-1 hours"));
 $end = date("D, d M Y H:i:00 +0800");
 
 $mg = Mailgun::create($config['api-key']);
-$result = $mg->events()->get($config['domain'], [
-    'begin' => $begin,
-    'end' => $end,
-    'ascending' => 'yes'
-])->getItems();
+try {
+    $result = fetchRecord($begin, $end);
+} catch (\Exception $e) {
+    try {
+        $result = fetchRecord($begin, $end);
+    } catch (\Exception $e) {
+        callSlack('zd_web_team', $e->getMessage());
+    }
+}
 
 if (empty($result)) {
     exit();
@@ -33,4 +38,37 @@ foreach ($result as $row) {
         continue;
     }
     echo $row->getEventDate()->format('Y-m-d H:i:s') . "\tSend\t" . json_encode($row->getMessage()['headers'], JSON_UNESCAPED_UNICODE) . "\n";
+}
+
+function callSlack($channel, $message)
+{
+    global $config;
+
+    if (!isset($config['hook-url']) || !isset($config['base-uri'])) {
+        // 沒有設定 hook-url 直接 return
+        return;
+    }
+    $hook_url = $config['hook-url'] . $channel;
+
+    try {
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => $config['base-uri']
+        ]);
+        $client->request("POST", $hook_url, [
+            'body' => $message
+        ]);
+    } catch (\Exception $e) {
+        // ignore slack pusher timeout exception
+    }
+}
+
+function fetchRecord($begin, $end)
+{
+    global $mg, $config;
+
+    return $mg->events()->get($config['domain'], [
+        'begin' => $begin,
+        'end' => $end,
+        'ascending' => 'yes'
+    ])->getItems();
 }
